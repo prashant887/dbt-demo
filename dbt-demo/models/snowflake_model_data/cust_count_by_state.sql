d02f50bf9acbe6cf96c3ce39bb214a4cc763ad1b
pl465j <pl465j@att.com>
2021-09-30 23:17:24 +0530

    added test cases

--- dbt-demo/models/snowflake_model_data/cust_count_by_state.sql
+++ dbt-demo/models/snowflake_model_data/cust_count_by_state.sql
@@ -2,4 +2,4 @@ with store_sales as (
     select * from {{ source('sales_data', 'customers') }}
 ),
 final as (select * from store_sales)
-select CUSTOMER_STATE,count(*) "CUST_COUNT" from final group by CUSTOMER_STATE
+select CUSTOMER_STATE,count(*) as CUST_COUNT from final group by CUSTOMER_STATE
